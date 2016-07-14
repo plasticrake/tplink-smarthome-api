a7d88e82bacf1d2015a42ae57733d97c3e9289ea
Patrick Seal <code@plasticrake.com>
2016-07-14 10:10:43 -0700

    test cleanup

--- test/lib/config.js
+++ test/lib/config.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Configuration used by some tests. Will turn the device off/on.
 const config = {host: '10.0.1.58'};
 
 module.exports = config;
--- test/utils.js
+++ test/utils.js
@@ -9,10 +9,10 @@ const encryptWithHeader = require('../utils').encryptWithHeader;
 const decrypt = require('../utils').decrypt;
 
 var payloads = {
-  on: ['{"system":{"set_relay_state":{"state":1}}}', 'AAAAKtDygfiL/5r31e+UtsWg1Iv5nPCR6LfEsNGlwOLYo4HyhueT9tTu36Lfog=='],
-  off: ['{"system":{"set_relay_state":{"state":0}}}', 'AAAAKtDygfiL/5r31e+UtsWg1Iv5nPCR6LfEsNGlwOLYo4HyhueT9tTu3qPeow=='],
-  getState: ['{ "system":{ "get_sysinfo":null } }', 'AAAAI9Dw0qHYq9+61/XPtJS20bTAn+yV5o/hh+jK8J7rh+vLtpbr'],
-  checkConsumption: ['{ "emeter":{ "get_realtime":null } }', 'AAAAJNDw0rfav8uu3P7Ev5+92r/LlOaD4o76k/6buYPtmPSYuMXlmA==']
+  setPowerStateOn: ['{"system":{"set_relay_state":{"state":1}}}', 'AAAAKtDygfiL/5r31e+UtsWg1Iv5nPCR6LfEsNGlwOLYo4HyhueT9tTu36Lfog=='],
+  setPowerStateOff: ['{"system":{"set_relay_state":{"state":0}}}', 'AAAAKtDygfiL/5r31e+UtsWg1Iv5nPCR6LfEsNGlwOLYo4HyhueT9tTu3qPeow=='],
+  getSysInfo: ['{ "system":{ "get_sysinfo":null } }', 'AAAAI9Dw0qHYq9+61/XPtJS20bTAn+yV5o/hh+jK8J7rh+vLtpbr'],
+  getConsumption: ['{ "emeter":{ "get_realtime":null } }', 'AAAAJNDw0rfav8uu3P7Ev5+92r/LlOaD4o76k/6buYPtmPSYuMXlmA==']
 };
 
 function decodeAndDecrypt (input) {
@@ -25,32 +25,32 @@ function encryptAndEncode (input) {
 
 describe('utils', function () {
   describe('#decrypt', function () {
-    it('should decode and decrypt setState ON payload', function () {
-      decodeAndDecrypt(payloads.on[1]).should.eql(payloads.on[0]);
+    it('should decrypt setState ON payload', function () {
+      decodeAndDecrypt(payloads.setPowerStateOn[1]).should.eql(payloads.setPowerStateOn[0]);
     });
-    it('should decode and decrypt setState OFF payload', function () {
-      decodeAndDecrypt(payloads.off[1]).should.eql(payloads.off[0]);
+    it('should decrypt setState OFF payload', function () {
+      decodeAndDecrypt(payloads.setPowerStateOff[1]).should.eql(payloads.setPowerStateOff[0]);
     });
     it('should decode and decrypt getState payload', function () {
-      decodeAndDecrypt(payloads.getState[1]).should.eql(payloads.getState[0]);
+      decodeAndDecrypt(payloads.getSysInfo[1]).should.eql(payloads.getSysInfo[0]);
     });
     it('should decode and decrypt usage payload', function () {
-      decodeAndDecrypt(payloads.checkConsumption[1]).should.eql(payloads.checkConsumption[0]);
+      decodeAndDecrypt(payloads.getConsumption[1]).should.eql(payloads.getConsumption[0]);
     });
   });
 
   describe('#encryptWithHeader', function () {
-    it('should encode and encrypt setState ON payload', function () {
-      encryptAndEncode(payloads.on[0]).should.eql(payloads.on[1]);
+    it('should encrypt setState ON payload', function () {
+      encryptAndEncode(payloads.setPowerStateOn[0]).should.eql(payloads.setPowerStateOn[1]);
     });
-    it('should encode and encrypt setState OFF payload', function () {
-      encryptAndEncode(payloads.off[0]).should.eql(payloads.off[1]);
+    it('should encrypt setState OFF payload', function () {
+      encryptAndEncode(payloads.setPowerStateOff[0]).should.eql(payloads.setPowerStateOff[1]);
     });
-    it('should encode and encrypt getState payload', function () {
-      encryptAndEncode(payloads.getState[0]).should.eql(payloads.getState[1]);
+    it('should encrypt getState payload', function () {
+      encryptAndEncode(payloads.getSysInfo[0]).should.eql(payloads.getSysInfo[1]);
     });
-    it('should encode and encrypt usage payload', function () {
-      encryptAndEncode(payloads.checkConsumption[0]).should.eql(payloads.checkConsumption[1]);
+    it('should encrypt usage payload', function () {
+      encryptAndEncode(payloads.getConsumption[0]).should.eql(payloads.getConsumption[1]);
     });
   });
 });
