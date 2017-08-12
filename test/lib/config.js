fac1db4ae777803ef932fa1e3b4431c8767b799d
Patrick Seal <code@plasticrake.com>
2017-08-11 21:30:14 -0700

    Use dotenv for test config

--- test/lib/config.js
+++ test/lib/config.js
@@ -1,8 +1,10 @@
 'use strict';
 
+require('dotenv').config();
+
 const client = { debug: false };
 // Configuration used by some tests. Will turn the device off/on.
-const plug = { host: '10.0.0.60', port: 9999 };
+const plug = { host: process.env.TEST_PLUG_HOST, port: 9999 };
 
 const invalidPlug = { deviceId: 'invalidPlug', host: '1.2.3.4', timeout: 1000 };
 
