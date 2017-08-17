6ec2a79cd04a85e3494ef5416dbdc2f50ccf5bef
Patrick Seal <code@plasticrake.com>
2017-08-17 16:15:14 -0700

    Update examples Rename Plug to Device

--- example/events.js
+++ example/events.js
@@ -2,19 +2,19 @@ const Hs100Api = require('../../hs100-api');
 
 const client = new Hs100Api.Client();
 
-var logEvent = function (eventName, plug) {
-  console.log(`${(new Date()).toISOString()} ${eventName} ${plug.model} ${plug.host} ${plug.deviceId}`);
+var logEvent = function (eventName, device) {
+  console.log(`${(new Date()).toISOString()} ${eventName} ${device.model} ${device.host} ${device.deviceId}`);
 };
 
-client.on('plug-new', (plug) => {
-  logEvent('plug-new', plug);
-  plug.on('power-on', (plug) => { logEvent('power-on', plug); });
-  plug.on('power-off', (plug) => { logEvent('power-off', plug); });
-  plug.on('in-use', (plug) => { logEvent('in-use', plug); });
-  plug.on('not-in-use', (plug) => { logEvent('not-in-use', plug); });
+client.on('device-new', (device) => {
+  logEvent('device-new', device);
+  device.on('power-on', (device) => { logEvent('power-on', device); });
+  device.on('power-off', (device) => { logEvent('power-off', device); });
+  device.on('in-use', (device) => { logEvent('in-use', device); });
+  device.on('not-in-use', (device) => { logEvent('not-in-use', device); });
 });
-client.on('plug-online', (plug) => { logEvent('plug-online', plug); });
-client.on('plug-offline', (plug) => { logEvent('plug-offline', plug); });
+client.on('device-online', (device) => { logEvent('device-online', device); });
+client.on('device-offline', (device) => { logEvent('device-offline', device); });
 
-console.log('Starting Plug Discovery');
+console.log('Starting Device Discovery');
 client.startDiscovery();
