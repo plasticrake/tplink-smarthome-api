e74cf19460ffedcde2b5bd4ee73c711c0105ad99
Patrick Seal <code@plasticrake.com>
2017-08-17 16:15:14 -0700

    Update examples Rename Plug to Device

--- example/plug.js
+++ example/plug.js
@@ -1,5 +1,5 @@
 const Hs100Api = require('../../hs100-api');
 
 const client = new Hs100Api.Client();
-const plug = client.getPlug({host: '10.0.0.60'});
-plug.getInfo().then(console.log);
+const device = client.getDevice({host: '10.0.0.60'});
+device.getInfo().then(console.log);
