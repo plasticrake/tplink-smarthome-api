947882e4ce67bb6ef666e7672f55ca136baa3e9d
Simon Christmann <simon@christmann.email>
2017-09-15 15:54:25 +0200

    Add event for consumption update
    Why not publish, when we collected the data anyway

--- example/events.js
+++ example/events.js
@@ -8,12 +8,17 @@ var logEvent = function (eventName, device) {
 
 client.on('device-new', (device) => {
   logEvent('device-new', device);
+  device.startPolling(5000);
+
   device.on('power-on', (device) => { logEvent('power-on', device); });
   device.on('power-off', (device) => { logEvent('power-off', device); });
   device.on('power-update', (device, powerOn) => { logEvent('power-update', device); });
+
   device.on('in-use', (device) => { logEvent('in-use', device); });
   device.on('not-in-use', (device) => { logEvent('not-in-use', device); });
   device.on('in-use-update', (device, inUse) => { logEvent('in-use-update', device); });
+
+  device.on('consumption-update', (device, consumption) => { logEvent('consumption-update', device); });
 });
 client.on('device-online', (device) => { logEvent('device-online', device); });
 client.on('device-offline', (device) => { logEvent('device-offline', device); });
