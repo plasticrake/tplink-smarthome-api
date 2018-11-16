906a551bcc275c0b5bdb590413ac8d76d821c1b5
S. Spain <sspain@users.noreply.github.com>
2018-11-15 22:18:14 -0700

    Upload shared/emeter.js
    Fix for lack of emeter on polling

--- src/shared/emeter.js
+++ src/shared/emeter.js
@@ -3,8 +3,11 @@
 /**
  * Eemter
  */
+
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
 class Emeter {
-  constructor (device, apiModuleName) {
+  constructor(device, apiModuleName) {
     this.device = device;
     this.apiModuleName = apiModuleName;
 
@@ -14,14 +17,17 @@ class Emeter {
    * Returns cached results from last retrieval of `emeter.get_realtime`.
    * @return {Object}
    */
-  get realtime () {
+  get realtime() {
     return this._realtime;
   }
   /**
    * @private
    */
-  set realtime (realtime) {
-    let normalize = function (propName, propName2, multiplier) {
+  set realtime(realtime) {
+    let normalize = function normalize(propName, propName2, multiplier) {
+      if (null == realtime) {
+        return null;
+      }
       if (realtime[propName] != null && realtime[propName2] == null) {
         realtime[propName2] = Math.floor(realtime[propName] * multiplier);
       } else if (realtime[propName] == null && realtime[propName2] != null) {
@@ -45,11 +51,15 @@ class Emeter {
    * @param  {SendOptions}  [sendOptions]
    * @return {Promise<Object, ResponseError>} parsed JSON response
    */
-  async getRealtime (sendOptions) {
-    this.realtime = await this.device.sendCommand({
-      [this.apiModuleName]: {get_realtime: {}}
-    }, sendOptions);
-    return this.realtime;
+  getRealtime(sendOptions) {
+    var _this = this;
+
+    return _asyncToGenerator(function* () {
+      _this.realtime = yield _this.device.sendCommand({
+        [_this.apiModuleName]: { get_realtime: {} }
+      }, sendOptions);
+      return _this.realtime;
+    })();
   }
   /**
    * Get Daily Emeter Statisics.
@@ -60,10 +70,14 @@ class Emeter {
    * @param  {SendOptions} [sendOptions]
    * @return {Promise<Object, ResponseError>} parsed JSON response
    */
-  async getDayStats (year, month, sendOptions) {
-    return this.device.sendCommand({
-      [this.apiModuleName]: { get_daystat: { year, month } }
-    }, sendOptions);
+  getDayStats(year, month, sendOptions) {
+    var _this2 = this;
+
+    return _asyncToGenerator(function* () {
+      return _this2.device.sendCommand({
+        [_this2.apiModuleName]: { get_daystat: { year, month } }
+      }, sendOptions);
+    })();
   }
   /**
    * Get Monthly Emeter Statisics.
@@ -73,10 +87,14 @@ class Emeter {
    * @param  {SendOptions} [sendOptions]
    * @return {Promise<Object, ResponseError>} parsed JSON response
    */
-  async getMonthStats (year, sendOptions) {
-    return this.device.sendCommand({
-      [this.apiModuleName]: { get_monthstat: { year } }
-    }, sendOptions);
+  getMonthStats(year, sendOptions) {
+    var _this3 = this;
+
+    return _asyncToGenerator(function* () {
+      return _this3.device.sendCommand({
+        [_this3.apiModuleName]: { get_monthstat: { year } }
+      }, sendOptions);
+    })();
   }
   /**
    * Erase Emeter Statistics.
@@ -85,10 +103,14 @@ class Emeter {
    * @param  {SendOptions} [sendOptions]
    * @return {Promise<Object, ResponseError>} parsed JSON response
    */
-  async eraseStats (sendOptions) {
-    return this.device.sendCommand({
-      [this.apiModuleName]: { erase_emeter_stat: {} }
-    }, sendOptions);
+  eraseStats(sendOptions) {
+    var _this4 = this;
+
+    return _asyncToGenerator(function* () {
+      return _this4.device.sendCommand({
+        [_this4.apiModuleName]: { erase_emeter_stat: {} }
+      }, sendOptions);
+    })();
   }
 }
 
