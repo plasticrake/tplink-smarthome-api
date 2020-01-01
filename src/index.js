3759d9e622cf4e2e991b790bf4a81a274ff9dcbc
plasticrake <code@plasticrake.com>
2019-12-31 23:22:06 -0800

    feat: export Bulb, Device, Plug

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
+const Bulb = require('./bulb');
+const Device = require('./device');
 const Client = require('./client');
+const Plug = require('./plug');
 const { ResponseError } = require('./utils');
 
 /**
  * @module tplink-smarthome-api
  */
 module.exports = {
+  Bulb,
   Client,
+  Device,
+  Plug,
   ResponseError,
 };
