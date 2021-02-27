342fdd0e275a4a6f3adc9191dcfadad0b54eca55
Patrick Seal <code@plasticrake.com>
2021-02-27 14:47:34 -0800

    docs: fix example that broke at v3
    fixes #116

--- examples/plug-turn-on-off.js
+++ examples/plug-turn-on-off.js
@@ -3,5 +3,13 @@ const { Client } = require('..'); // or require('tplink-smarthome-api')
 const client = new Client();
 
 const plug = client.getPlug({ host: '10.0.0.60' });
-plug.setPowerState(true);
-plug.setPowerState(false);
+
+client.getDevice({ host: '10.0.0.60' }).then((device) => {
+  console.log('Found device:', device.deviceType, device.alias);
+
+  if (device.deviceType === 'plug') {
+    console.log('Turning plug on, then off', device.alias);
+    device.setPowerState(true);
+    device.setPowerState(false);
+  }
+});
