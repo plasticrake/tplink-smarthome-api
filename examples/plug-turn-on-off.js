851a0f69f30bc6c8d15efb395fd6ed1c24d32096
Patrick Seal <code@plasticrake.com>
2021-02-27 15:19:25 -0800

    docs: fix example lint

--- examples/plug-turn-on-off.js
+++ examples/plug-turn-on-off.js
@@ -2,8 +2,6 @@ const { Client } = require('..'); // or require('tplink-smarthome-api')
 
 const client = new Client();
 
-const plug = client.getPlug({ host: '10.0.0.60' });
-
 client.getDevice({ host: '10.0.0.60' }).then((device) => {
   console.log('Found device:', device.deviceType, device.alias);
 
